1a71f8ab28f094b9d6da44e1b31ee5d1b99d0fca
FAIZAH AHSAN <faizah1992@gmail.com>
2020-02-24 22:14:12 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,7 +11,7 @@ end
 
 def monopoly_with_third_tier
   hash = monopoly_with_second_tier
-  hash[:railroads][:rent_in_dollars]={:a=>"faizah",:b=>"ahsan",:c=>"farshid",:d=>"zaman"}
+  hash[:railroads][:rent_in_dollars]={:one_piece_owned=>25}
   hash[:railroads][:names]={:a=>"faizah",:b=>"ahsan",:c=>"farshid",:d=>"zaman"}
   hash 
 end
