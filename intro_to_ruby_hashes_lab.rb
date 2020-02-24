e6cb125bb901cae78a868dca57b7f44d8cf1119e
FAIZAH AHSAN <faizah1992@gmail.com>
2020-02-24 22:24:10 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,5 +19,6 @@ end
 def monopoly_with_fourth_tier
   hash=monopoly_with_third_tier
   hash[:railroads][:names][:reading_railroad]['mortgage_value']="$100"
+  hash[:railroads][:names][:pennsylvania_railroad]['mortgage_value']="$200"
   hash
 end
